f106e90407265ed14f9709b59d25c7ff9155b249
aramyamal <aram@aramjamal.com>
2025-09-01 20:50:28 +0200

    feat(vector): implement VoidVector API

--- src/vector.c
+++ src/vector.c
@@ -1,51 +1,93 @@
 #include "aramlib/vector.h"
+#include "aramlib/array.h"
+#include <stdbool.h>
+#include <stddef.h>
+
+static inline VoidVector VoidVector_null(void) { return (VoidVector){0}; }
+
+static inline bool VoidVector_is_valid(const VoidVector *self) {
+    return self && VoidArray_is_valid(&self->array);
+}
+
+static inline bool VoidVector_is_valid_index(const VoidVector *self,
+                                             size_t index) {
+    return self && index < self->length &&
+           VoidArray_is_valid_index(&self->array, index);
+}
 
 VoidVector VoidVector_create(size_t capacity, size_t element_size) {
-    // TODO
-    VoidVector vec = {0};
-    return vec;
+    if (capacity == 0) {
+        return VoidVector_null();
+    }
+    VoidArray array = VoidArray_create(capacity, element_size);
+    if (!VoidArray_is_valid(&array)) {
+        return VoidVector_null();
+    }
+    return (VoidVector){
+        .array = array,
+        .length = 0,
+    };
 }
 
 void VoidVector_destroy(VoidVector *self) {
-    // TODO
-}
-
-size_t VoidVector_length(const VoidVector *self) {
-    // TODO
-    return 0;
-}
-
-size_t VoidVector_capacity(const VoidVector *self) {
-    // TODO
-    return 0;
+    VoidArray_destroy(&self->array);
+    self->length = 0;
 }
 
 bool VoidVector_push(VoidVector *self, const void *value) {
-    // TODO
+    if (!value || !VoidVector_is_valid(self)) {
+        return false;
+    }
+    size_t length = self->length;
+    if (length >= self->array.length) {
+        size_t new_capacity = length * 2;
+        if (!VoidArray_resize(&self->array, new_capacity)) {
+            return false; // failed to resize
+        }
+    }
+    if (!VoidArray_set(&self->array, value, length)) {
+        return false;
+    };
+    self->length++;
     return true;
 }
 
 bool VoidVector_get(const VoidVector *self, void *out_value, size_t index) {
-    // TODO
-    return false;
+    return VoidVector_is_valid_index(self, index)
+               ? VoidArray_get(&self->array, out_value, index)
+               : false;
 }
 
 bool VoidVector_set(VoidVector *self, const void *value, size_t index) {
-    // TODO
-    return false;
+    return VoidVector_is_valid_index(self, index)
+               ? VoidArray_set(&self->array, value, index)
+               : false;
 }
 
 bool VoidVector_pop(VoidVector *self, void *out_value) {
-    // TODO
-    return false;
+    size_t length = VoidVector_length(self);
+    if (!out_value || length == 0 || !VoidVector_is_valid(self)) {
+        return false;
+    }
+    if (!VoidArray_get(&self->array, out_value, length - 1)) {
+        return false;
+    }
+    self->length--;
+    return true;
 }
 
 void VoidVector_clear(VoidVector *self) {
-    // TODO
+    if (VoidVector_is_valid(self)) {
+        self->length = 0;
+    }
 }
 
 VoidVector VoidVector_copy(const VoidVector *self) {
-    // TODO
-    VoidVector vec = {0};
-    return vec;
+    if (!VoidVector_is_valid(self)) {
+        return VoidVector_null();
+    }
+    return (VoidVector){
+        .array = VoidArray_copy(&self->array),
+        .length = self->length,
+    };
 }
